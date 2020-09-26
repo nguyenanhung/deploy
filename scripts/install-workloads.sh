50760a21ece759b01dc813a18521527e0d0b87c0
quangbm <quangbm@gviet.vn>
2020-09-26 15:37:19 +0700

    [UPDATE] move consul rancher-apps to core

--- scripts/install-workloads.sh
+++ scripts/install-workloads.sh
@@ -17,9 +17,20 @@ workloads=( \
   "srs-ocm" \
   "srs-origin" \
   "traefik-ingress-controller" \
+  "consul-controller-sync-k8s" \
+  )
+
+builded_workloads=( \
+  "consul-controller-client" \
+  "consul-controller-server" \
   )
 
 for workload in ${workloads[@]};
 do
   kustomize build ./src/core/$workload/overlays/$env | kubectl apply -f -
+done
+
+for workload in ${builded_workloads[@]};
+do
+  kubectl apply -f ./src/core/$workload/build/${env}.yaml
 done
---
+++ src/core/consul-controller-client/README.md
@@ -0,0 +1,6 @@
+# consul-controller-client
+
+* dependencies:
+  * consul-controller-server
+
+because of kustomize's bug (https://github.com/kubernetes-sigs/kustomize/issues/2767), we have to have an addition folder (build) to store the builded manifest. If the bug is fixed, build folder will be deleted in the future
